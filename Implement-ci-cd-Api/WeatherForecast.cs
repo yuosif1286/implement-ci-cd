9e88cd590b311b8faf67b1a55bde2b9dded1a56b
yuosif1286 <84059356+yuosif1286@users.noreply.github.com>
2024-08-05 15:22:13 +0300

    add new message

---
+++ Implement-ci-cd-Api/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿namespace Implement_ci_cd_Api;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
+
